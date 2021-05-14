df3ec62de07392ed2bab85c7f634048c5c8987b0
Ayo Olaifa <ayoolaifa@gmail.com>
2021-05-14 15:14:07 +0100

    Changing Theme Of Application (Buttons And Text)

--- app/src/main/java/com/example/tictactoe/HelpAndTutorial.java
+++ app/src/main/java/com/example/tictactoe/HelpAndTutorial.java
@@ -17,7 +17,7 @@ public class HelpAndTutorial extends AppCompatActivity {
                 "<b><li>The game is played on a grid that's 3 squares by 3 squares.</li></b>\n" +
                 "<b><li>You are X, your friend or the Computer is O.</li></b>\n" +
                 "<b><li>Players take turns putting their marks in empty squares.</li></b>\n" +
-                "<b><li>The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.</li></b>\n" +
+                "<b><li>The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.</li></b>\n" +
                 "<b><li>When all 9 squares are full, the game is over.</li></b>\n" +
                 "<b><li>If no player has 3 marks in a row, the game ends in a tie.</li></b>";
 
